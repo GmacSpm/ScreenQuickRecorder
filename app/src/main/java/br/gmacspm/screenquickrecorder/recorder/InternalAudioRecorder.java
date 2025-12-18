3f485e1627ff10f43257ae485bec50fb4a7a114b
GmacSpm <giovanemacedo25@gmail.com>
2025-12-18 19:17:32 -0300

    Removed unused method of ADTS

--- app/src/main/java/br/gmacspm/screenquickrecorder/recorder/InternalAudioRecorder.java
+++ app/src/main/java/br/gmacspm/screenquickrecorder/recorder/InternalAudioRecorder.java
@@ -155,25 +155,6 @@ public class InternalAudioRecorder {
         }
     }
 
-    private byte[] addADTStoPacket(int packetLen) {
-        byte[] packet = new byte[7];
-
-        int profile = 2; // AAC LC
-        int freqIdx = 4; // 44100 Hz
-        int chanCfg = 2; // stereo
-
-        packet[0] = (byte) 0xFF;
-        packet[1] = (byte) 0xF9;
-        packet[2] = (byte) (((profile - 1) << 6) + (freqIdx << 2) + (chanCfg >> 2));
-        packet[3] = (byte) (((chanCfg & 3) << 6) + (packetLen >> 11));
-        packet[4] = (byte) ((packetLen & 0x7FF) >> 3);
-        packet[5] = (byte) (((packetLen & 7) << 5) + 0x1F);
-        packet[6] = (byte) 0xFC;
-
-        return packet;
-    }
-
-
     public void stopInternalAudioCapture() {
         isCapturing = false;
 
